555f0ca46987dd37dd294c116f65527979df5149
enerzilius <113470774+enerzilius@users.noreply.github.com>
2023-08-19 23:57:40 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ranking-tenis-mouraoense',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
