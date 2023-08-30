5cea6be52a9a3b9ad887e8d8b2557dc9c893af8a
enerzilius <113470774+enerzilius@users.noreply.github.com>
2023-08-30 01:11:37 -0300

    arrumando a estrutura

---
+++ src/app/landing-page/landing-page.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: 'landing-page.page.html',
+  styleUrls: ['landing-page.page.scss']
+})
+export class LandingPage {
+
+  constructor() {}
+
+}
